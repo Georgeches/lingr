0245ca743ccb1864817515bd4ccf991dad43273f
Georgeches <georgechesire83@gmail.com>
2023-06-13 17:45:25 +0300

    hide the bottom bar on bigger devices

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -1,12 +1,13 @@
 import './BottomBar.css'
+import {Link} from 'react-router-dom'
 
 function BottomBar(){
     console.log("bottom-bar")
     return(
-        <div className="bottom-bar" style={{height:`${window.screen.height*0.05}px`, width:`${window.screen.width}px`, top:`${window.screen.height*0.74}px`}}>
-            <button><span class="material-symbols-outlined">home</span></button>
+        <div className="bottom-bar" style={{height:`${window.screen.height*0.05}px`, width:`${window.screen.width}px`, top:`${window.screen.height*0.72}px`}}>
+            <button><Link to="/"><span class="material-symbols-outlined">home</span></Link></button>
             <button><i class="las la-search"></i></button>
-            <button><span class="material-symbols-outlined">filter_list</span></button>
+            <button><Link to="/list"><span class="material-symbols-outlined">filter_list</span></Link></button>
             <button><i class="las la-comment"></i></button>
             <button><span class="material-symbols-outlined">account_circle</span></button>
         </div>
